73ca7c86b45ab2983558f503ad4c8be33392e19b
Timi <timiplayskeys@gmail.com>
2022-08-10 10:25:59 -0400

    Added the markers to the currentlocation with direction

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,3 +1,6 @@
+//Where are you going today? BERG map can show you the way :)
+//Happy Coding!
+
 mapboxgl.accessToken = 'pk.eyJ1Ijoic2FoZWJiaGFsbGEiLCJhIjoiY2w2bXg2MXYwMDM3ZzNxcWoxYzdmbjhkeSJ9.5HnzmM4U9a2dpxbBlxO4mA';
 
  
@@ -15,19 +18,32 @@ function locationError(){
 }
 
 
+
+
 //To set the location to the map
 function setLocationToMap(position){
     const map = new mapboxgl.Map({
         container: 'map', // container ID
-        style: 'mapbox://styles/mapbox/streets-v11', // style URL
+        style: 'mapbox://styles/mapbox/streets-v11', // style URL can also be used to change styles
         center: position, // starting position [lng, lat]
-        zoom: 16, // starting zoom
+        zoom: 14, // starting zoom
         projection: 'globe' // display the map as a 3D globe
         });
     
     map.on('style.load', () => {
         map.setFog({}); // Set the default atmosphere style
         });
+
+  //Controls the marker 
+    map.addControl(new mapboxgl.GeolocateControl({
+            positionOptions: {
+                enableHighAccuracy: true
+            },
+            trackUserLocation: true,
+            showUserHeading: true
+        }));
+
+
 
     //Buttons to zoom in and out of the map and rotation controls to the map.
     map.addControl(new mapboxgl.NavigationControl());
@@ -40,5 +56,9 @@ function setLocationToMap(position){
         }),
         'top-left'
         );
+
 }
+
+
+    
 
